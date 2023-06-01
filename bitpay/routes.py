c3756e630666032dc2134884d392f64162d7c600
Eduard Kelmann <eduard@kelmann.net>
2023-06-02 00:12:42 +0200

    add skeletons & jwt example

--- bitpay/routes.py
+++ bitpay/routes.py
@@ -1,8 +1,14 @@
-from fastapi import HTTPException, APIRouter
+from fastapi import HTTPException, APIRouter, Depends
 from pydantic import BaseModel
 from bitpay.services.wallet import Wallet
+from bitpay.services.config_manager import ConfigManager
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
+from jwt.exceptions import InvalidTokenError
 
 router = APIRouter()
+security = HTTPBearer()
+config = ConfigManager()
 
 class Transaction(BaseModel):
     recipient: str
@@ -11,32 +17,43 @@ class Transaction(BaseModel):
 class User(BaseModel):
     user_id: str
 
+def authenticate_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        token = credentials.credentials
+        payload = jwt.decode(token, config.read_config("JWT", "secret_key"), algorithms=["HS256"])
+        user_id = payload.get("user_id")
+        if user_id:
+            return User(user_id=user_id)
+    except InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
 @router.post("/create_key")
-def create_key(user: User):
+def create_key(user: User = Depends(authenticate_user)):
     wallet = Wallet(user.user_id)
     wallet.create_key()
     return {"message": "Key created"}
 
 @router.delete("/delete_key")
-def delete_key(user: User):
+def delete_key(user: User = Depends(authenticate_user)):
     wallet = Wallet(user.user_id)
     wallet.delete_key()
     return {"message": "Key deleted"}
 
 @router.get("/get_balance")
-def get_balance(user: User):
+def get_balance(user: User = Depends(authenticate_user)):
     wallet = Wallet(user.user_id)
     balance = wallet.get_balance()
     return {"balance": balance}
 
 @router.get("/get_transaction_history")
-def get_transaction_history(user: User):
+def get_transaction_history(user: User = Depends(authenticate_user)):
     wallet = Wallet(user.user_id)
     transactions = wallet.get_transaction_history()
     return {"transactions": transactions}
 
 @router.post("/send_transaction")
-def send_transaction(user: User, transaction: Transaction):
+def send_transaction(user: User = Depends(authenticate_user), transaction: Transaction):
     wallet = Wallet(user.user_id)
     try:
         wallet.send_transaction(transaction.recipient, transaction.amount)
@@ -45,13 +62,13 @@ def send_transaction(user: User, transaction: Transaction):
     return {"message": "Transaction sent"}
 
 @router.get("/get_transaction_status")
-def get_transaction_status(user: User, tx_hash: str):
+def get_transaction_status(user: User = Depends(authenticate_user), tx_hash: str):
     wallet = Wallet(user.user_id)
     tx_status = wallet.get_transaction_status(tx_hash)
     return {"transaction_status": tx_status}
 
 @router.get("/get_transaction_fee")
-def get_transaction_fee(user: User, tx_hash: str):
+def get_transaction_fee(user: User = Depends(authenticate_user), tx_hash: str):
     wallet = Wallet(user.user_id)
     tx_fee = wallet.get_transaction_fee(tx_hash)
     return {"transaction_fee": tx_fee}
