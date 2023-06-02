3eda28e88ac67506768c9799d46d4d5e48a43934
Eduard Kelmann <eduard@kelmann.net>
2023-06-02 22:49:36 +0200

    codestyle fix

--- bitpay/models/user.py
+++ bitpay/models/user.py
@@ -4,9 +4,11 @@ from pydantic import BaseModel
 class User(BaseModel):
     username: str
 
+
 class UserRegister(BaseModel):
     username: str
     password: str
+
 
 class UserLogin(BaseModel):
     username: str
